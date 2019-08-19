aee3097b3c4d31a68a77c508e19a96ae34ec4502
Paul Tobin <tobster111@gmail.com>
2019-08-19 19:06:39 +0100

    Setup with mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI:
+    'mongodb+srv://tobsirl:HkQ3zzqGMkApisSP@advancednodejsconcepts-4te5z.mongodb.net/test?retryWrites=true&w=majority',
   cookieKey: '123123123'
 };
