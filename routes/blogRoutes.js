dbe175acaa5ca2f258598a9d70dd17566e13e87f
Paul Tobin <tobster111@gmail.com>
2019-08-21 14:31:35 +0100

    Using JSON stringify + parse to handle the objects in redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -24,13 +24,17 @@ module.exports = app => {
     const cachedBlogs = await client.get(req.user.id);
 
     // if yes, then respond to the request right away and return
-
+    if (cachedBlogs) {
+      return res.send(JSON.parse(cachedBlogs));
+    }
     // if no, we need to respond to the request and update our cache
     // to store the data
 
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
