a07d590fecafbc47902ba30d6df3ba0e4031f31c
Paul Tobin <tobster111@gmail.com>
2019-08-22 15:00:44 +0100

    Using the Users id to create a unique hash key for redis

--- services/cache.js
+++ services/cache.js
@@ -4,12 +4,14 @@ const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get); // promisify client.get
+client.hget = util.promisify(client.hget); // promisify client.get
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
+
   return this;
 };
 
@@ -25,7 +27,7 @@ mongoose.Query.prototype.exec = async function() {
   );
 
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do, return that
   if (cacheValue) {
@@ -40,6 +42,6 @@ mongoose.Query.prototype.exec = async function() {
 
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  client.hset(this.hashKey, key, JSON.stringify(result));
   return result;
 };
