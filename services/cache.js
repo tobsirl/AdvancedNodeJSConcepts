e383d613f087382d1cd79c5698875a9adaec91fb
Paul Tobin <tobster111@gmail.com>
2019-08-22 12:23:45 +0100

    Checking to see if the data is in redis, if so returning it from the data store

--- services/cache.js
+++ services/cache.js
@@ -8,16 +8,27 @@ client.get = util.promisify(client.get); // promisify client.get
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-  console.log("I'm about to run a query");
+mongoose.Query.prototype.exec = async function() {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  // console.log(this.getQuery());
-  // console.log(this.mongooseCollection.name);
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name
-  });
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
 
-  console.log(key);
+  // If we do, return that
+  if (cacheValue) {
+    const doc = new this.model(JSON.parse(cacheValue));
 
-  return exec.apply(this, arguments);
+    return doc;
+  }
+
+  // Otherwise, issue the query and store the result in redis
+
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+  return result;
 };
